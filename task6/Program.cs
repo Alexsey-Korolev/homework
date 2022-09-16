9d00cb1377185bca3fdd128122c4d8d23b2a8e5c
Aleksei <spacerabbit1@mail.ru>
2022-09-16 13:00:59 +0700

    homework

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.Write("Да.");
+}
+else
+{
+    Console.Write("Нет.");
+}
---
+++ task8/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int x = 2; x < N+1; x++)       
+if (x % 2==0)            
+Console.WriteLine(x);
